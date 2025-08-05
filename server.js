007121fec31621a85f263f8357747fbe1e20b90e
MusfiqueUsSalehin <musfiqueishti@gmail.com>
2025-08-06 04:43:31 +0600

    Initial commit - user profile backend

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 4321;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.use("/api/users", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
