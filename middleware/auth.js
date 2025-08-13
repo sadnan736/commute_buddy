5b74992c43aa9659dcdd79e79c26056580a90e0a
Mushfiq2002 <pratulkhan2015@gmail.com>
2025-08-13 18:22:56 +0600

    backend code of module 1 for rahman

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,19 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/users");
 
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(403).json({ error: "Token missing" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const user = await User.findById(decoded.id).select("-password");
+    
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+    
+    req.user = user;
     next();
   } catch (err) {
     res.status(401).json({ error: "Invalid token" });
