5debe6334dd7bcbee015bfc8a2168883b81ed8cc
Sadnan Islam <sadnan15916q@gmail.com>
2025-09-02 03:12:54 +0600

    add admin routes

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 
 
 const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 const map_routes = require("./routes/map_routes");
 const report_routes = require("./routes/report_routes")
 
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/map", map_routes);
 app.use("/api/reports",report_routes)
 
